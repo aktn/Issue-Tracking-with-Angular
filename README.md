9e4020e7a80934d1f006bcae0dec216e025ac82d
Aung Khant <theaungkhant@gmail.com>
2017-11-15 18:49:35 +0000

    package installation

---
+++ README.md
@@ -0,0 +1,9 @@
+brew install yarn // For mac 
+
+OR 
+
+npm install -g yarn
+
+yarn install
+
+yarn start
---
+++ src/app/containers/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div>
+      Issue Tracking System
+    </div>
+  `
+})
+export class AppComponent {
+  constructor() {}
+}
